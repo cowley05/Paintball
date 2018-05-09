21c805f07aabd8385fb62a6ecf4e987d7871994d
Ste <cowley05@hotmail.co.uk>
2018-05-09 22:02:06 +0100

    Ste - Adds content loading to index html

--- src/assets/js/combatpaintball.js
+++ src/assets/js/combatpaintball.js
@@ -11,8 +11,22 @@ $(function () {
 });
 
 function loadContent() {
-    $('.github-content').each(function () {
-        let contentFile = $(this).data("content-file");
-        $(this).load("https://raw.githubusercontent.com/cowley05/Paintball/master/src/content/" + contentFile + ".html?cb=" + new Date().getTime())
-    })
+    // $('.github-content').each(function () {
+    //     let contentFile = $(this).data("content-file");
+    //     $(this).load("https://raw.githubusercontent.com/cowley05/Paintball/master/src/content/" + contentFile + ".html?cb=" + new Date().getTime())
+    // })
+
+    $.get("https://raw.githubusercontent.com/cowley05/Paintball/master/src/content/content.json", function (data) {
+        let json = JSON.parse(data);
+        loadSpecialOffersBanner(json.bannerSpecialOffers);
+    });
+
+    function loadSpecialOffersBanner(offers) {
+        offers.forEach(function (offer) {
+            let item = document.getElementById(offer.id);
+            item.getElementsByTagName("h1")[0].innerHTML = offer.heading;
+            item.getElementsByTagName("p")[0].innerHTML = offer.text;
+            console.log(offer.text);
+        });
+    }
 }
