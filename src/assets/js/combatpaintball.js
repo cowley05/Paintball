3b5aae0cde936e7f9753e09d8b9c9a3128058ee3
Ste <cowley05@hotmail.co.uk>
2018-05-24 21:26:12 +0100

    Ste - Generates offer details content from json

--- src/assets/js/combatpaintball.js
+++ src/assets/js/combatpaintball.js
@@ -11,14 +11,11 @@ $(function () {
 });
 
 function loadContent() {
-    // $('.github-content').each(function () {
-    //     let contentFile = $(this).data("content-file");
-    //     $(this).load("https://raw.githubusercontent.com/cowley05/Paintball/master/src/content/" + contentFile + ".html?cb=" + new Date().getTime())
-    // })
 
     $.get("https://raw.githubusercontent.com/cowley05/Paintball/master/src/content/content.json", function (data) {
         let json = JSON.parse(data);
         loadSpecialOffersBanner(json.bannerSpecialOffers);
+        loadSpecialOfferDetails(json.specialOfferDetails);
     });
 
     function loadSpecialOffersBanner(offers) {
@@ -28,5 +25,34 @@ function loadContent() {
             item.getElementsByTagName("p")[0].innerHTML = offer.text;
             console.log(offer.text);
         });
+    }
+
+    function loadSpecialOfferDetails(offers) {
+        offers.forEach(function (offerData) {
+            let offerDiv = createElementWithClass('div', 'row featurette');
+
+            let offerDivInner = createElementWithClass('div', 'col-md-12');
+            offerDiv.appendChild(offerDivInner);
+
+            let offerH2 = createElementWithClass('h2', 'featurette-heading');
+            let headingText = document.createTextNode(offerData.heading);
+            offerH2.appendChild(headingText);
+            offerDivInner.appendChild(offerH2);
+
+            let offerP = createElementWithClass('p', 'lead');
+            let pText = document.createTextNode(offerData.text);
+            offerP.appendChild(pText);
+            offerDivInner.appendChild(offerP);
+
+            let offerDetailsDiv = document.getElementById('offer-details');
+            offerDetailsDiv.appendChild(offerDiv);
+
+            function createElementWithClass(tagName, className) {
+                let element = document.createElement(tagName);
+                element.className = className;
+                return element;
+            }
+        });
+
     }
 }
